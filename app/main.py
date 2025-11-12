87157c3b9b81e22874b4878d8f8f055017b842bd
Kasjan Michalak <kasjan.michalak@gmail.com>
2025-11-12 21:59:21 +0000

    last save

--- app/main.py
+++ app/main.py
@@ -1,21 +1,24 @@
 def format_linter_error(error: dict) -> dict:
-    return {nowy: error[stary] for nowy, stary in
-        {"line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
-        "source" : "flake8"}.items()
-        }
+    return {
+        nowy: error[stary]
+        for nowy, stary in {
+            "line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"
+        }.items()
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "passed" if errors == [] else "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path = path, errors = errors)
+    return [format_single_linter_file(file_path=path, errors=errors)
             for path, errors in linter_report.items()]
