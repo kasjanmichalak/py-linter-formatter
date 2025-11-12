053f8c5d997f3c33aeaec305d8f745c5b9395c59
Kasjan Michalak <kasjan.michalak@gmail.com>
2025-11-12 16:02:23 +0000

    First save

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    mapa = {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        }
+    result = {nowy: error[stary] for nowy, stary in mapa.items()}
+    result["source"] = "flake8"
+    return result
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    mapa = {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
+    result = [{nowy: error[stary] for nowy, stary in mapa.items()}] in errors
+    result["path"] = file_path
+    result["status"] = "failed"
+    return result
 
 
 def format_linter_report(linter_report: dict) -> list:
